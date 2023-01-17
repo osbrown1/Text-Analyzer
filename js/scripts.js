e69f140e4020de8c1665587b0ad659dee3732db8
osbrown1 <socknot@live.com>
2023-01-17 15:45:00 -0800

    Document TDD process on wordfunction
    Co-authored-by: Jannon Sielaff <jannon.sielaff@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// Business Logic
+
+
+
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function(word) {
+    if (!Number(word)) {
+    wordCount++;
+  }
+  });
+  return wordCount;
+}
