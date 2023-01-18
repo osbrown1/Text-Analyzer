076a34ee2d05dff71d2ff0c41a370e2deacac556
osbrown1 <socknot@live.com>
2023-01-17 16:22:47 -0800

    Further TDD testing for text analyzing function
    Co-authored-by: Jannon Sielaff <jannon.sielaff@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 // Business Logic
 
-
-
 function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
@@ -16,3 +14,19 @@ function wordCounter(text) {
   });
   return wordCount;
 }
+
+
+// const text = "red blue red red red green";
+// const word = "red";
+
+function numberOfOccurrencesInText(word, text) {
+  const textArray = text.split(" ");
+  let wordCount = 0;
+  textArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+  
